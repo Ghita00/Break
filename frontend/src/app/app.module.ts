21610d4e56dfca9122871de702e0efe16b3086db
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-07-11 23:17:52 +0200

    added material icon, completed design login page and footer I will fix css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   currentPage: "colleghi" | "home" | "login" | "ristoranti" | "" = "";
-  statusMenu: Boolean = true;
+  statusMenu: Boolean = false;
 
   constructor() { }
 
